69e6d245efb9823b307cf7a60fb833be13e7424b
noprom <tyee.noprom@qq.com>
2015-06-07 18:07:53 +0800

    add MessageModel
    add MessageModel

---
+++ src/main/java/com/huntdreams/wei/model/BaseListModel.java
@@ -0,0 +1,27 @@
+package com.huntdreams.wei.model;
+
+import android.os.Parcelable;
+
+import java.util.List;
+
+/**
+ * 信息列表模型接口
+ *
+ * @author noprom (https://github.com/noprom)
+ * @version 1.0
+ * Created by noprom on 2015/6/7.
+ */
+public interface BaseListModel<ITEM, LIST> extends Parcelable {
+    public int total_number = 0;
+    public String previous_cursor = null, next_cursor = null;
+
+    public int getSize();
+    public ITEM get(int position);
+    public List<ITEM> getList();
+
+    /**
+     * @param toTop If true, add to top, else add to bottom
+     * @param values All values needed to be added
+     */
+    public void addAll(boolean toTop, LIST values);
+}
---
+++ src/main/java/com/huntdreams/wei/model/MessageListModel.java
@@ -0,0 +1,11 @@
+package com.huntdreams.wei.model;
+
+/**
+ * 消息列表Model
+ *
+ * @author noprom (https://github.com/noprom)
+ * @version 1.0
+ * Created by noprom on 2015/6/7.
+ */
+public class MessageListModel {
+}
