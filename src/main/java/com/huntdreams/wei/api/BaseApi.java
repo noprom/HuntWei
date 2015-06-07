083934de05c0ffefa7838044cac3512e6a3e0498
noprom <tyee.noprom@qq.com>
2015-06-07 10:50:31 +0800

    add login api constants
    add login api constants

---
+++ src/main/java/com/huntdreams/wei/api/BaseApi.java
@@ -0,0 +1,33 @@
+package com.huntdreams.wei.api;
+
+import com.sina.weibo.sdk.auth.WeiboParameters;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Api基类
+ *
+ * @author noprom (https://github.com/noprom)
+ * @version 1.0
+ *          Created by noprom on 2015/6/7.
+ */
+public class BaseApi {
+    private static final String TAG = BaseApi.class.getSimpleName();
+
+    // Http methods
+    protected static final String HTTP_GET = "GET";
+    protected static final String HTTP_POST = "POST";
+
+    // Access token
+    private static String mAccessToken;
+
+    protected static JSONObject request(String url, WeiboParameters params, String method)throws JSONException {
+        if(mAccessToken == null){
+            return null;
+        }else{
+            params.put("access_token", mAccessToken);
+            String jsonData = AsyncWeiboRunner
+        }
+    }
+}
---
+++ src/main/java/com/huntdreams/wei/api/Constants.java
@@ -0,0 +1,15 @@
+package com.huntdreams.wei.api;
+
+/**
+ * API接口常量
+ *
+ * @author noprom (https://github.com/noprom)
+ * @version 1.0
+ * Created by noprom on 2015/6/7.
+ */
+public class Constants {
+
+    public static final String SINA_BASE_URL = "https://api.weibo.com/2/";
+    // Login
+    public static final String OAUTH2_ACCESS_TOKEN = SINA_BASE_URL + "oauth2/access_token";
+}
