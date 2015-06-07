c29b7fa3caf39900babc2160a9f8eeceb6fecce2
noprom <tyee.noprom@qq.com>
2015-06-07 14:46:43 +0800

    Add uid to LoginApiCache
    Add uid to LoginApiCache

--- src/main/java/com/huntdreams/wei/cache/login/LoginApiCache.java
+++ src/main/java/com/huntdreams/wei/cache/login/LoginApiCache.java
@@ -4,7 +4,9 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.util.Log;
 
+import com.huntdreams.wei.api.BaseApi;
 import com.huntdreams.wei.api.login.LoginApi;
+import com.huntdreams.wei.api.user.AccountApi;
 
 import static com.huntdreams.wei.BuildConfig.DEBUG;
 /**
@@ -20,16 +22,22 @@ public class LoginApiCache {
 
     private SharedPreferences mPrefs;
     private String mAccessToken;
+    private String mUid;
     private long mExpireDate;
     private String mAppId;
     private String mAppSecret;
 
     public LoginApiCache(Context context){
         mPrefs = context.getSharedPreferences("access_token",Context.MODE_PRIVATE);
-        mAccessToken = mPrefs.getString("access_token" ,null);
+        mAccessToken = mPrefs.getString("access_token", null);
+        mUid = mPrefs.getString("uid", null);
         mExpireDate = mPrefs.getLong("expires_in", Long.MIN_VALUE);
         mAppId = mPrefs.getString("app_id", null);
         mAppSecret = mPrefs.getString("app_secret", null);
+
+        if(mAccessToken != null){
+            BaseApi.setmAccessToken(mAccessToken);
+        }
     }
 
     public void login(String appId, String appSecret, String username,String password){
@@ -44,6 +52,8 @@ public class LoginApiCache {
                     Log.d(TAG, "result got, loading to cache");
                 }
                 mAccessToken = result[0];
+                BaseApi.setmAccessToken(mAccessToken);
+                mUid = AccountApi.getUid();
                 mExpireDate = System.currentTimeMillis() + Long.valueOf(result[1]);
                 mAppId = appId;
                 mAppSecret = appSecret;
@@ -55,12 +65,13 @@ public class LoginApiCache {
     public void logout(){
         mAccessToken = null;
         mExpireDate = Long.MIN_VALUE;
-        mPrefs.edit().remove("access_token").remove("expires_in").commit();
+        mPrefs.edit().remove("access_token").remove("expires_in").remove("uid").commit();
     }
 
     public void cache(){
         mPrefs.edit().putString("access_token", mAccessToken)
                 .putLong("expires_in", mExpireDate)
+                .putString("uid", mUid)
                 .putString("app_id", mAppId)
                 .putString("app_secret", mAppSecret)
                 .commit();
@@ -68,6 +79,10 @@ public class LoginApiCache {
 
     public String getAccessToken() {
         return mAccessToken;
+    }
+
+    public String getUid() {
+        return mUid;
     }
 
     public long getExpireDate() {
