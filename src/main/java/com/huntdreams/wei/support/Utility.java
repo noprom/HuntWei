b930938bbe3885c8899ddc253a8da6423a50f5d5
noprom <tyee.noprom@qq.com>
2015-06-07 11:49:18 +0800

    add two methods in Utility
    add two methods in Utility

---
+++ src/main/java/com/huntdreams/wei/support/Utility.java
@@ -0,0 +1,31 @@
+package com.huntdreams.wei.support;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 自定义工具类
+ *
+ * @author noprom (https://github.com/noprom)
+ * @version 1.0
+ * Created by noprom on 2015/6/7.
+ */
+public class Utility {
+
+    /**
+     * 返回即将多少天后过期
+     * @param time
+     * @return
+     */
+    public static int expireTimeInDays(long time){
+        return (int) TimeUnit.MILLISECONDS.toDays(time - System.currentTimeMillis());
+    }
+
+    /**
+     * 判断Token是否过期
+     * @param time
+     * @return
+     */
+    public static boolean isTokenExpired(long time){
+        return time <= System.currentTimeMillis();
+    }
+}
