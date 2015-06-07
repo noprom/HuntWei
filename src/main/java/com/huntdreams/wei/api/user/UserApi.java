abd3774ac5d03d01f0630414b1f8c82d43b77641
noprom <tyee.noprom@qq.com>
2015-06-07 15:01:52 +0800

    UserApi done
    UserApi done

--- src/main/java/com/huntdreams/wei/api/user/UserApi.java
+++ src/main/java/com/huntdreams/wei/api/user/UserApi.java
@@ -1,7 +1,17 @@
 package com.huntdreams.wei.api.user;
 
-import com.huntdreams.wei.api.BaseApi;
+import android.util.Log;
 
+import com.google.gson.Gson;
+import com.huntdreams.wei.api.BaseApi;
+import com.huntdreams.wei.api.Constants;
+import com.huntdreams.wei.model.UserModel;
+import com.sina.weibo.sdk.net.WeiboParameters;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import static com.huntdreams.wei.BuildConfig.DEBUG;
 /**
  * 用户个人信息API接口
  *
@@ -13,5 +23,19 @@ public class UserApi extends BaseApi{
 
     private static final String TAG = UserApi.class.getSimpleName();
 
-    public static
+    public static UserModel getUser(String uid){
+        WeiboParameters params = new WeiboParameters();
+        params.put("uid", uid);
+
+        try{
+            JSONObject json = request(Constants.USER_SHOW, params, HTTP_GET);
+            UserModel user = new Gson().fromJson(json.toString(), UserModel.class);
+            return user;
+        } catch (JSONException e) {
+            if(DEBUG){
+                Log.e(TAG, "Failed to fetch user info from net: " + e.getClass().getSimpleName());
+            }
+            return null;
+        }
+    }
 }
